ddd76f2568064b5bf8e2d67e459c88c22a579712
Daniel Carey <whawke8@hotmail.com>
2020-01-09 21:12:22 -0600

    Cleaned up the code.

--- Models/ColorPalette.cs
+++ Models/ColorPalette.cs
@@ -6,42 +6,32 @@ using System.Drawing;
 using System.ComponentModel;
 
 namespace slack_theme_builder_blazor.Models {
-    public class ColorPalette : INotifyPropertyChanged {
 
-        private Color _columnBG;
-        public Color ColumnBG { get => _columnBG; set => SetProperty(value, ref _columnBG); }
+    public class ColorPalette {
 
-        private Color _menuBGHover;
-        public Color MenuBGHover { get => _menuBGHover; set => SetProperty(value, ref _menuBGHover); }
+        public ColorPalette() {
 
-        private Color _activeItem;
-        public Color ActiveItem { get => _activeItem; set => SetProperty(value, ref _activeItem); }
+        }
 
-        private Color _activeItemText;
-        public Color ActiveItemText { get => _activeItemText; set => SetProperty(value, ref _activeItemText); }
+        public ColorPalette(ColorPalette palette) {
+            ColumnBG = palette.ColumnBG;
+            MenuBGHover = palette.MenuBGHover;
+            ActiveItem = palette.ActiveItem;
+            ActiveItemText = palette.ActiveItemText;
+            HoverItem = palette.HoverItem;
+            TextColor = palette.TextColor;
+            ActivePresence = palette.ActivePresence;
+            MentionBadge = palette.MentionBadge;
+        }
 
-        private Color _hoverItem;
-        public Color HoverItem { get => _hoverItem; set => SetProperty(value, ref _hoverItem); }
-
-        private Color _textColor;
-        public Color TextColor { get => _textColor; set => SetProperty(value, ref _textColor); }
-
-        private Color _activePresence;
-        public Color ActivePresence { get => _activePresence; set => SetProperty(value, ref _activePresence); }
-
-        private Color _mentionBadge;
-        public Color MentionBadge { get => _mentionBadge; set => SetProperty(value, ref _mentionBadge); }
-
-        //public override string ToString() => string.Join(',', 
-        //    ColorTranslator.ToHtml(ColumnBG),
-        //    ColorTranslator.ToHtml(MenuBGHover),
-        //    ColorTranslator.ToHtml(ActiveItem),
-        //    ColorTranslator.ToHtml(ActiveItemText),
-        //    ColorTranslator.ToHtml(HoverItem),
-        //    ColorTranslator.ToHtml(TextColor),
-        //    ColorTranslator.ToHtml(ActivePresence),
-        //    ColorTranslator.ToHtml(MentionBadge)
-        //);
+        public Color ColumnBG { get; set; } = Color.Olive;
+        public Color MenuBGHover { get; set; } = Color.Blue;
+        public Color ActiveItem { get; set; } = Color.DarkKhaki;
+        public Color ActiveItemText { get; set; } = Color.DarkSeaGreen;
+        public Color HoverItem { get; set; } = Color.LightSeaGreen;
+        public Color TextColor { get; set; } = Color.AliceBlue;
+        public Color ActivePresence { get; set; } = Color.YellowGreen;
+        public Color MentionBadge { get; set; } = Color.LightYellow;
 
         public string Text => string.Join(',',
                 ColorTranslator.ToHtml(ColumnBG),
@@ -53,19 +43,6 @@ namespace slack_theme_builder_blazor.Models {
                 ColorTranslator.ToHtml(ActivePresence),
                 ColorTranslator.ToHtml(MentionBadge)
             );
-
-
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void SetProperty<T>(T value, ref T field, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "") {
-            if (!Equals(field, value)) {
-                field = value;
-                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
-
     }
-};
+}
 
--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
-# Blazor notes
+# Blazor sample of slack-theme-builder
+https://github.com/aharvard/slack-theme-builder
+
+## Running the project
+* Install the [.NET Core SDK](https://dotnet.microsoft.com/download/dotnet-core/3.1)
+* From the shell:
+   dotnet build
+   dotnet run
+
+* Navigate to either:
+  http://localhost:5000
+  https://localhost:5001
 
 ## Creating the project
 dotnet new blazorserver
@@ -11,10 +22,16 @@ dotnet new blazorserver
 
 ## Github issues
 
-Blazor currently doesn't support css isloation;i.e. per component. It is
-being looked at!
-[CSS Isolation in Blazor Components](https://github.com/dotnet/aspnetcore/issues/10170)
+Two-way binding for input type Color isn't built-in yet. I worked around that by one-way binding the 
+value and handling the onchange event that in turn, calls the callback method.
 [Two way binding does not yet handle color picker...](https://github.com/dotnet/aspnetcore/issues/10376)
+
+
+
+Blazor currently doesn't support css isloation;i.e. per component styles. It is being looked at!
+[CSS Isolation in Blazor Components](https://github.com/dotnet/aspnetcore/issues/10170)
+
+
 
 
 
